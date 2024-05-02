3a3c93ff96809e6f6e68427d28d22521a94d6e05
Erwin Jansen <echjansen@outlook.com>
2024-05-02 17:37:45 +1000

    Added install_snapper().

--- pure-arch.sh
+++ pure-arch.sh
@@ -1839,18 +1839,28 @@ function copy_logs() {
     fi
 }
 
-function snapper() {
-    umount /.snapshots/
-    rm -r /.snapshots/
-    snapper -c root create-config /
-    btrfs subvol delete /.snapshots
-    mkdir /.snapshots
-    mount -av
-    btrfs subvol set-def 256 /
-    chown -R :wheel /.snapshots
-    snapper -c root create -d "*** Pure-Arch Installed ***"
-    grub-mkconfig -o /boot/grub/grub.cfg
-    systemctl enable --now grub-btrfsd
+function snapper_install() {
+    #umount /.snapshots/
+    umount "${MNT_DIR}"/.snapshots/
+    #rm -r /.snapshots/
+    rm -r "${MNT_DIR}"/.snapshots/
+    #snapper -c root create-config /
+    snapper -c root create-config "${MNT_DIR}"
+    #btrfs subvol delete /.snapshots
+    btrfs subvol delete "${MNT_DIR}"/.snapshots
+    #mkdir /.snapshots
+    mkdir "${MNT_DIR}"/.snapshots
+    #mount -av
+    arch-chroot "${MNT_DIR}" mount -av
+    #btrfs subvol set-def 256 /
+    btrfs subvol set-def 256 "${MNT_DIR}" 
+    #chown -R :wheel /.snapshots
+    chown -R :wheel "${MNT_DIR}"/.snapshots
+    #snapper -c root create -d "*** Pure-Arch Installed ***"
+    arch-chroot snapper -c root create -s "**** Pure-Arch Installed ****"
+    #grub-mkconfig -o /boot/grub/grub.cfg
+    arch-chroot grub-mkconfig -o /boot/grub/grub.cfg
+    #systemctl enable --now grub-btrfsd
 }
 
 function main() {
@@ -1906,6 +1916,11 @@ function main() {
         execute_step "vagrant"
     fi
     execute_step "systemd_units"
+
+    if [ "$FILE_SYSTEM_TYPE" == "btrfs" ]; then
+        execute_step "snapper_install"
+    fi
+
     local END_TIMESTAMP=$(date -u +"%F %T")
     local INSTALLATION_TIME=$(date -u -d @$(($(date -d "$END_TIMESTAMP" '+%s') - $(date -d "$START_TIMESTAMP" '+%s'))) '+%T')
     echo -e "Installation start ${WHITE}$START_TIMESTAMP${NC}, end ${WHITE}$END_TIMESTAMP${NC}, time ${WHITE}$INSTALLATION_TIME${NC}"
