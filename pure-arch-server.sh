5fcfbf82b1c9ceb4ba6a9137d12b2a2acc812f9f
echjansen <155590081+echjansen@users.noreply.github.com>
2024-05-09 11:38:58 +1000

    Update pure-arch-server.sh
    Added virtual machine detection

--- pure-arch-server.sh
+++ pure-arch-server.sh
@@ -512,6 +512,29 @@ sed -i 's/022/077/g' /mnt/etc/profile
 echo "" >> /mnt/etc/bash.bashrc
 echo "umask 077" >> /mnt/etc/bash.bashrc
 
+hypervisor=$(systemd-detect-virt)
+case $hypervisor in
+    kvm )   info_print "KVM has been detected, setting up guest tools."
+            pacstrap /mnt qemu-guest-agent &>/dev/null
+            systemctl enable qemu-guest-agent --root=/mnt &>/dev/null
+            ;;
+    vmware  )   info_print "VMWare Workstation/ESXi has been detected, setting up guest tools."
+                pacstrap /mnt open-vm-tools >/dev/null
+                systemctl enable vmtoolsd --root=/mnt &>/dev/null
+                systemctl enable vmware-vmblock-fuse --root=/mnt &>/dev/null
+                ;;
+    oracle )    info_print "VirtualBox has been detected, setting up guest tools."
+                pacstrap /mnt virtualbox-guest-utils &>/dev/null
+                systemctl enable vboxservice --root=/mnt &>/dev/null
+                ;;
+    microsoft ) info_print "Hyper-V has been detected, setting up guest tools."
+                pacstrap /mnt hyperv &>/dev/null
+                systemctl enable hv_fcopy_daemon --root=/mnt &>/dev/null
+                systemctl enable hv_kvp_daemon --root=/mnt &>/dev/null
+                systemctl enable hv_vss_daemon --root=/mnt &>/dev/null
+                ;;
+esac
+
 # Finishing up
 info_print "Done, you may now wish to reboot (further changes can be done by chrooting into /mnt)."
 exit
