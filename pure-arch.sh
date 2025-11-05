33e452bc2c7d74f4fe76e0881ff13180735b555e
echjansen <echjansen@outlook.com>
2025-11-05 13:38:36 +1100

    Added -n to echo for password piping.

--- pure-arch.sh
+++ pure-arch.sh
@@ -1371,11 +1371,11 @@ function install_user() {
     # run "arch-chroot ${MOUNT_POINT} /bin/bash -c \"echo '${USER_NAME}:${USER_PASS_HASHED}' | chpasswd -e\""
 
     # Creating the password without run - to bypass any issues on piping
-    echo '${USER_NAME}:${USER_PASS_HASHED}' | chpasswd -e
+    echo -n '${USER_NAME}:${USER_PASS_HASHED}' | arch-chroot ${MOUNT_POINT} chpasswd -e
 
     # 1. Create the user, with root privileges and home directory
     run "arch-chroot ${MOUNT_POINT} useradd -G wheel -s ${USER_SHELL} -m bob"
-    echo 'bob:123' | arch-chroot ${MOUNT_POINT} chpasswd
+    echo -n 'bob:123' | arch-chroot ${MOUNT_POINT} chpasswd
 
     # Allow the WHEEL group to run sudo commands, without providing password
     run "cp -f rootfs/etc/sudoers ${MOUNT_POINT}/etc/sudoers"
